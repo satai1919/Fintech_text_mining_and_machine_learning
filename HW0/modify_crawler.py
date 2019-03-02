61ef14b2f357aeb74cceacbc19cd9618f02782c8
satai1919 <love9420101@gmail.com>
2019-03-02 15:03:17 +0800

    add lv3.py

---
+++ HW0/modify_crawler.py
@@ -0,0 +1,93 @@
+'''
+from https://github.com/MiccWan/Political-News-Analysis/blob/master/crawler/new_talk_crawler.ipynb
+for homework usage.
+'''
+
+import sys
+import pickle
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+def get_date(news_block_node):
+	'''
+	找到新聞中的發布日期，並轉成正確的格式
+	'''
+    date_string = news_block_node.find(class_="news_date").string.split('|')[0][2:-1]
+    return(datetime.strptime(date_string, '%Y.%m.%d').strftime('%Y-%m-%d'))
+    
+def get_title(news_block_node):
+	'''
+	找到新聞中的標題
+	'''
+    return news_block_node.find(class_="news_title").a.string
+
+def get_link(news_block_node):
+	'''
+	找到新聞的網址
+	'''
+    return news_block_node.find(class_="news_title").a.get('href')
+
+def get_content(link):
+	'''
+	找到新聞內文
+	'''
+    r = requests.get(link)
+    r.encoding = "UTF-8"
+    soup = BeautifulSoup(r.text, 'html.parser')
+    article_node = soup.find(itemprop='articleBody')
+    article = article_node.get_text()
+    return article.replace("\n", "")
+
+def get_news_info(each_news):
+	'''
+	對每則新聞做處理，找到需要的資料
+	'''
+    date  = get_date(each_news)
+    title = get_title(each_news)
+    link  = get_link(each_news)
+    content = get_content(link)
+    
+    info = {'date' : date,
+            'title': title,
+            'link' : link,
+            'content': content}
+    return(info)
+
+def get_page_news(page_url):
+	'''
+	把需要的新聞搜尋頁碼轉成需要的格式，開始抓取每一連結的新聞內容
+	'''
+    r = requests.get(page_url)
+    r.encoding = "UTF-8"
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    news_blocks = soup.find_all(class_="news-list-item clearfix ")
+    
+    news = []
+    for each_news in news_blocks:
+        try:
+            news_info = get_news_info(each_news)
+        except:
+        	#無論因為甚麼緣故失敗，就直接略過
+            pass
+
+        news.append(news_info)
+    return(news)
+
+def get_new_talk_news(from_page=1, end_page=270, url="https://newtalk.tw/news/subcategory/2/政治/"):
+    '''
+	從指定的起始於結束頁碼之間找尋資料（預設1-269頁、政治版）
+    '''
+    print("page_number from {} to {}".format(from_page, end_page -1))
+    data = []
+    for page_number in range(from_page, end_page):
+        print("page_number: {}".format(page_number))
+        #一頁一頁的處理
+        data = data + get_page_news( url+str(page_number) )
+    
+    print('done')
+    return(data)
+
+
+data = get_new_talk_news(from_page=1, end_page=270)
